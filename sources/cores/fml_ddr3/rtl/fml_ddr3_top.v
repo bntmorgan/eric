70a6d511677b25e097606f6353bc7f933a0226de
Benoît Morgan <bmorgan@laas.fr>
2014-07-29 16:55:27 +0200

    Instanciation de l'ipcore MIG dans fml_ddr3_top

--- sources/cores/fml_ddr3/rtl/fml_ddr3_top.v
+++ sources/cores/fml_ddr3/rtl/fml_ddr3_top.v
@@ -104,4 +104,49 @@ fml_ddr3_ctlif #(
   .app_wdf_wren(app_wdf_wren)
 );
 
+`ifndef SIMULATION
+
+// MIG ipcore instaciation
+mig ddr3 (
+  ddr3_sys_clk_p(ddr3_sys_clk_p),
+  ddr3_sys_clk_n(ddr3_sys_clk_n),
+  ddr3_clk_ref_p(ddr3_clk_ref_p),
+  ddr3_clk_ref_n(ddr3_clk_ref_n),
+  ddr3_dq(ddr3_dq),
+  ddr3_addr(ddr3_addr),
+  ddr3_ba(ddr3_ba),
+  ddr3_ras_n(ddr3_ras_n),
+  ddr3_cas_n(ddr3_cas_n),
+  ddr3_we_n(ddr3_we_n),
+  ddr3_reset_n(ddr3_reset_n),
+  ddr3_cs_n(ddr3_cs_n),
+  ddr3_odt(ddr3_odt),
+  ddr3_cke(ddr3_cke),
+  ddr3_dm(ddr3_dm),
+  ddr3_dqs_p(ddr3_dqs_p),
+  ddr3_dqs_n(ddr3_dqs_n),
+  ddr3_ck_p(ddr3_ck_p),
+  ddr3_ck_n(ddr3_ck_n),
+  sda(sda),
+  scl(scl),
+  app_wdf_wren(app_wdf_wren),
+  app_wdf_data(app_wdf_data),
+  app_wdf_mask(app_wdf_mask),
+  app_wdf_end(app_wdf_end),
+  app_addr(app_addr),
+  app_cmd(app_cmd),
+  app_en(app_en),
+  app_rdy(app_rdy),
+  app_wdf_rdy(app_wdf_rdy),
+  app_rd_data(app_rd_data),
+  app_rd_data_end(app_rd_data_end),
+  app_rd_data_valid(app_rd_data_valid),
+  ui_clk_sync_rst(ui_clk_sync_rst),
+  ui_clk(ui_clk),
+  phy_init_done(phy_init_done),
+  ddr3_sys_rst(ddr3_sys_rst)
+);
+
+`endif
+
 endmodule
